69d4f9519d9b3a1cdd0850a983571e4ae1062945
Tauqeer Ahmed <freelancingtwin@gmail.com>
2023-06-26 13:18:28 +0530

    pulled cors up the code to see it applies proper

--- server.js
+++ server.js
@@ -5,11 +5,6 @@ import 'dotenv/config'
 import path from 'path'
 const app = express();
 const PORT = process.env.PORT;
-const __dirname = path.resolve(path.dirname(''));
-app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public/assets')));
-app.use(express.static(path.join(__dirname, 'public/assets/fonts')));
 const corsOptions = {
   // origin: 'http://localhost:8080/',
   origin: "*",
@@ -24,8 +19,16 @@ app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Headers', true);
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
+const __dirname = path.resolve(path.dirname(''));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/assets')));
+app.use(express.static(path.join(__dirname, 'public/assets/fonts')));
+
+
 app.get('/', (req, res)=>{
   res.sendFile("/index.html")
 })
