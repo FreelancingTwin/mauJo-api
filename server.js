2690d5699311fb47aed7fff09c35a937fe64d1c3
Tauqeer Ahmed <freelancingtwin@gmail.com>
2023-06-26 13:31:19 +0530

    fonts test 1

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Origin', "https://apitouibleshooting--maujo.netlify.app");
   res.header('Access-Control-Allow-Headers', true);
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
